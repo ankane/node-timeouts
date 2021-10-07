ac677e40dd814eb1f5e31122ed6601f0cc4deaf9
Andrew Kane <andrew@ankane.org>
2021-10-06 21:58:52 -0700

    Fixed mongodb read test [skip ci]

--- test/mongodb.test.js
+++ test/mongodb.test.js
@@ -10,10 +10,10 @@ test('connect', async () => {
   await client.connect().catch(e => expect(e.message).toMatch('connection timed out'));
 });
 
-// TODO figure out how to close connection
-test.skip('read', async () => {
+test('read', async () => {
   expect.assertions(1);
   const client = new MongoClient('mongodb://' + helper.readHostAndPort(), {
+    connectTimeoutMS: 1000,
     socketTimeoutMS: 1000,
     serverSelectionTimeoutMS: 1000
   });
